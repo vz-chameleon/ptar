15560ce69f17d43c6bdecc197980d043bc7ad0f0
GTimothy <timothy.garwood@protonmail.com>
2016-10-24 02:46:04 +0200

    Added linkedList structure to store multiple tar headers

--- src/ptar.h
+++ src/ptar.h
@@ -10,25 +10,30 @@
 
 /*Structure for tar header in ustar format*/
 struct header_posix_ustar{
-	char File_name[100];
-	char File_mode[8];
-	char Owner_s_numeric_user_ID[8];
-	char Group_s_numeric_user_ID[8];
-	char File_size_in_bytes_octalB[12];
-	char Last_modification_time_in_numeric_Unix_time_format_OctalB[12];
-	char Checksum_for_header_record[8];
-	char Type_flag[1];
-	char Name_of_linked_file[100];
-	char UStar_indicator[6];
-	char UStar_version[2];
-	char Owner_user_name[32];
-	char Owner_group_name[32];
-	char Device_major_number[8];
-	char Device_minor_number[8];
-	char Filename_prefix[155];
-	}ptar_header;
+	char File_name[100]; 												//100
+	char File_mode[8];													//108
+	char Owner_s_numeric_user_ID[8];									//116
+	char Group_s_numeric_user_ID[8];									//124
+	char File_size_in_bytes_octalB[12];									//136
+	char Last_modification_time_in_numeric_Unix_time_format_OctalB[12];	//148
+	char Checksum_for_header_record[8];									//156
+	char Type_flag[1];													//157
+	char Name_of_linked_file[100];										//257
+	char UStar_indicator[6];											//263
+	char UStar_version[2];												//265
+	char Owner_user_name[32];											//297
+	char Owner_group_name[32];											//329
+	char Device_major_number[8];										//337
+	char Device_minor_number[8];										//345
+	char Filename_prefix[155];											//500
+	char rest_up_to_512[12];											//512
+	};
 
-
+/*Structure for tar header linked list*/
+struct header_posix_ustar_linkedList_node{
+	struct header_posix_ustar header;
+	struct header_posix_ustar_linkedList_node * next;
+};
 
 
 
